513d1c521e27b31b68b23380c09bd7a7e92b68c7
Ashish <ash.ishu025g@gmail.com>
2022-01-14 22:18:00 +0530

    first file

---
+++ HelloWorld.js
@@ -0,0 +1,13 @@
+console.log("Hello World From VS code using Node Server")
+
+var Name = "Cody";
+
+console.log(`Name ${Name}`)
+
+function PrintAge(age)
+{
+    console.log("Age "+ age)
+    return age;
+}
+
+PrintAge(21);
