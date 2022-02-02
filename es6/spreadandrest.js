23a31adbb9427944e082156ccb58580ec8996de6
Ashish <ash.ishu025g@gmail.com>
2022-02-02 22:42:16 +0530

    spread task

--- es6/spreadandrest.js
+++ es6/spreadandrest.js
@@ -49,3 +49,8 @@ Accounts.accountHolderName = "Hoaian";
 console.log(Accounts)
 
 console.log(PurhaseOrder)
+
+
+//create list of vaccines and print
+//create doctor object and print his qualifications and other details using spread
+//create a vaccine object with details like - name, no of doses required, price etc and merge it with nearest doctor object using spread
