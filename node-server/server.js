a7235366b4b927d59b6adb626566a08cb4ffa478
Ashish <ash.ishu025g@gmail.com>
2022-02-23 22:06:41 +0530

    node api start

--- node-server/server.js
+++ node-server/server.js
@@ -1,1 +1,15 @@
 console.log("This file is named as server.js")
+
+const express = require('express')
+const app = express()
+
+app.get('/getname', function (req, res) {
+    res.send('this is coming from getname api')
+  })
+
+app.get('/', function (req, res) {
+  res.send('Hello World - Cody, Kim, Tohney and Hoian')
+})
+
+console.log("Application is listening at port :3000 - localhost:3000")
+app.listen(3000)
