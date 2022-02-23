b75ae158e1500fa066cc8ac040042d915a43bf50
Ashish <ash.ishu025g@gmail.com>
2022-02-23 23:02:19 +0530

    nodemon

--- node-server/server.js
+++ node-server/server.js
@@ -2,14 +2,19 @@ console.log("This file is named as server.js")
 
 const express = require('express')
 const app = express()
+const port = 9000;
 
 app.get('/getname', function (req, res) {
     res.send('this is coming from getname api')
   })
 
+app.get('/gethtml', function (req, res) {
+    res.send("<h1>This is html<h1>")
+})
+
 app.get('/', function (req, res) {
   res.send('Hello World - Cody, Kim, Tohney and Hoian')
 })
 
-console.log("Application is listening at port :3000 - localhost:3000")
-app.listen(3000)
+console.log(`Application is listening at port :${port} - localhost:${port}`)
+app.listen(port)
