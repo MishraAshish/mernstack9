426ed74c71197c85ba698f4e88d0eee2cd1d5a3b
Ashish <ash.ishu025g@gmail.com>
2022-03-02 23:26:05 +0530

    webpack setup

---
+++ node-server/routes/webpack-stuffs.js
@@ -0,0 +1,16 @@
+//xyz.min.js - it will not be human readable format
+//asdas asdsa sdas asdasdas asdasd sadas asdasd bla bla bla bla bla bla bla bla  bla bla 
+
+//  let allowmetosignin = getsigininfo();
+//  if (allowmetosignin) {
+//      //i want to do something
+//  } else {
+//      //i want to do something else
+//  }  
+
+ //getsigininfo() ? i want to do something : i want to do something else;
+
+ //task runners - optimize our code with some task by configuring it - minification or uglyfication
+
+ //es5 - 
+ //transpilation - es6, react, jsx, typescript to es5, webpack task runner to use babel
---
+++ reactapp/src/index.js
@@ -0,0 +1,3 @@
+//this is the entry point of our application
+console.log("this is the entry file of our project")
+alert("Entry file!")
